6131d0f3df566b023a9c7c798402f96c31724267
BeYoddy <changwoon4@naver.com>
2023-02-21 01:13:53 +0900

    리펙토링

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let news = [];
+let url;
 let menus = document.querySelectorAll(".menus button");
 console.log("menus", menus);
 menus.forEach((menu) =>
@@ -7,49 +8,37 @@ menus.forEach((menu) =>
 
 let serchButton = document.getElementById("search-button");
 
+const getNews = async () => {
+  let header = new Headers({
+    "x-api-key": "IdK0gOCehLGzFjjcsbAeVTZP22L99ENbRHU60JYC6HA",
+  });
+  let response = await fetch(url, { headers: header });
+  let data = await response.json();
+  news = data.articles;
+
+  render();
+};
 const getLatestNews = async () => {
   url = new URL(
     "https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=2"
   );
-  let header = new Headers({
-    "x-api-key": "IdK0gOCehLGzFjjcsbAeVTZP22L99ENbRHU60JYC6HA",
-  });
-  let response = await fetch(url, { headers: header });
-  let data = await response.json();
-  news = data.articles;
-
-  render();
+  getNews();
 };
 
 const getNewsByTopic = async (event) => {
-  console.log("클릭됨", event.target.textContent);
   let topic = event.target.textContent.toLowerCase();
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=2&topic=${topic}`
   );
-  let header = new Headers({
-    "x-api-key": "IdK0gOCehLGzFjjcsbAeVTZP22L99ENbRHU60JYC6HA",
-  });
-  let response = await fetch(url, { headers: header });
-  let data = await response.json();
-  news = data.articles;
-
-  render();
+  getNews();
 };
 
 const getNewsByKeyword = async () => {
   let keyword = document.getElementById("search-input").value;
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`
   );
-  let header = new Headers({
-    "x-api-key": "IdK0gOCehLGzFjjcsbAeVTZP22L99ENbRHU60JYC6HA",
-  });
-  let response = await fetch(url, { headers: header });
-  let data = await response.json();
-  news = data.articles;
-
-  render();
+  getNews();
 };
 
 const render = () => {
